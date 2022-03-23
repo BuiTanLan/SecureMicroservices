20470a63aabc108afc0d87e7294a4c339e17bacd
Lân Bùi Tấn <tanlanhcmus@gmail.com>
2022-03-23 23:12:49 +0700

    feat(): Add ApiIdentityKey

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -1,11 +1,25 @@
+using Microsoft.IdentityModel.Tokens;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
-IConfiguration configuration = new ConfigurationBuilder()
+const string authenticationProviderKey = "IdentityApiKey";
+var configuration = new ConfigurationBuilder()
     .AddJsonFile("ocelot.json")
     .Build();
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOcelot(configuration);
+builder.Services.AddAuthentication()
+    .AddJwtBearer(authenticationProviderKey, x =>
+    {
+        x.Authority = "https://localhost:5005"; // IDENTITY SERVER URL
+        x.RequireHttpsMetadata = false;
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = false
+        };
+    });
+
 var app = builder.Build();
 app.UseOcelot();
 app.MapControllers();
